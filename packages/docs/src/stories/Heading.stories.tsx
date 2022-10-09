3cd074085b1b9137345cbae8aa24ea263aa5f7bd
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-10-09 18:55:32 -0300

    docs: adding parameters description

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -16,4 +16,12 @@ export const CustomElement: StoryObj<HeadingProps> = {
     children: 'H1 Heading',
     as: 'h1',
   },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Por padrão heading sempre será um `h2`, mas podemos alterar isso usando a propriedade `as`',
+      },
+    },
+  },
 }
