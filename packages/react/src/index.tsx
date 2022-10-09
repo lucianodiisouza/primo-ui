e86e681c32047a67eb4808f4c4f2b2b6ce8e6c32
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-10-08 22:30:20 -0300

    dev: config stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,11 @@
-import { colors } from '@primo-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$green500',
+  borderRadius: '$md',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.green500 }}>Hello World</h1>
+  return <Button>Hello</Button>
 }
