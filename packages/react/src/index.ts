851b8600d5f543625f1fc2905eaf7e9478356f0c
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-10-08 22:18:12 -0300

    dev: testing react pkg build

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@primo-ui/tokens'
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@primo-ui/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.green500 }}>Hello World</h1>
+}
