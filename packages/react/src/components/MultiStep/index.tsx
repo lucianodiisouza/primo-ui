c4818fc8a053046afa73bf4b79e5277362430a37
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-10-10 11:47:55 -0300

    feat: add MultiStep component

---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,31 @@
+import { ComponentProps } from 'react'
+import { Label, MultiStepContainer, Step, Steps } from './styles'
+
+export function MultiStep({
+  size,
+  currentStep = 1,
+  stepLabel = 'Passo',
+  ofLabel = 'de',
+}: MultiStepProps) {
+  return (
+    <MultiStepContainer>
+      <Label>
+        {stepLabel} {currentStep} {ofLabel} {size}
+      </Label>
+
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => {
+          return <Step key={step} active={currentStep >= step} />
+        })}
+      </Steps>
+    </MultiStepContainer>
+  )
+}
+
+export interface MultiStepProps
+  extends ComponentProps<typeof MultiStepContainer> {
+  size: number
+  currentStep?: number
+  stepLabel?: string
+  ofLabel?: string
+}
