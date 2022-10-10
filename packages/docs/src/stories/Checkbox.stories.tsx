3191de4ff41642dac765704f80af7f1e2dbb4e10
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-10-10 10:42:36 -0300

    docs: add Checkbox stories

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,29 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Box, Text, Checkbox, CheckboxProps } from '@primo-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Enable Notifications</Text>
+        </Box>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
+
+export const Disabled: StoryObj<CheckboxProps> = {
+  args: {
+    disabled: true,
+  },
+}
